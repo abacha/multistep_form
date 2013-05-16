2c01ab89cdbc79e341abff3d37922be4884a2185
Adriano Bacha <abacha@gmail.com>
2013-05-16 16:02:46 -0300

    adding first_step? and last_step? methods

--- multistep_form.rb
+++ multistep_form.rb
@@ -4,4 +4,24 @@ module MultistepForm
   def initialize
     raise StepsNotDefined unless defined?(steps)
   end
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
+
+  def force_step(options)
+    if options.is_a?(Hash)
+      @current_step = steps[options[:index]]
+    else
+      @current_step = options
+    end
+  end
+
+  def current_step
+    @current_step || steps.first
+  end
 end
--- specs/multistep_form_spec.rb
+++ specs/multistep_form_spec.rb
@@ -1,6 +1,17 @@
 require_relative "../multistep_form.rb"
 
+
+class Demo
+  include MultistepForm
+
+  def steps
+    %w[step1 step2 step3]
+  end
+end
+
 describe MultistepForm do
+  let(:demo) { Demo.new }
+  let(:steps) { %w[step1 step2 step3] }
 
   it "expects a 'steps' method" do
     expect { 
@@ -10,12 +21,17 @@ describe MultistepForm do
     }.to raise_error(MultistepForm::StepsNotDefined)
 
     expect { 
-      class Demo
-        include MultistepForm
-        def steps
-          {}
-        end
-      end.new
+      Demo.new
     }.not_to raise_error(MultistepForm::StepsNotDefined)
+  end
+
+  it "test steps" do
+    demo.first_step?.should be_true
+    demo.last_step?.should be_false
+    demo.force_step(index: 2)
+    demo.last_step?.should be_true
+    demo.force_step(steps[0])
+    demo.first_step?.should be_true
+    demo.last_step?.should be_false
   end
 end
